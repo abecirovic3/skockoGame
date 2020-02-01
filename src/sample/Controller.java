045622e3e168f4533ec405dcbb0fe2e8c1fa2be8
Ajdin Bečirović <abecirovic3@etf.unsa.ba>
2020-02-01 20:18:42 +0100

    styled game window

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label lab00;
+    public Label lab01;
+    public Label lab02;
+    public Label lab03;
+
+    @FXML
+    public void initialize() {
+        lab00.setStyle(null);
+        lab00.getStyleClass().add("fullHit");
+        lab01.getStyleClass().add("fullHit");
+        lab02.getStyleClass().add("fullHit");
+        lab03.getStyleClass().add("fullHit");
+    }
 }
